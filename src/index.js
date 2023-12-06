163c5a6054f185334cdbede5e1b5aa4aa739425a
Rogério Andrella <rogerioandrella24081990@gmail.com>
2023-12-06 14:55:48 -0300

    Chapter 7

--- src/index.js
+++ src/index.js
@@ -1,55 +1,41 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
-let notes = [
-    { id: '1', content: 'This is a note', author: 'Adam Scott' },
-    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
-];
+const app = express();
 
-const typeDefs = gql`
-    type Query {
-        hello: String!
-        notes: [Note!]!
-        note(id: ID!): Note!
-    }
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`;
+db.connect(DB_HOST);
 
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-        notes: () => notes,
-        note: (parent, args) => {
-            return notes.find(note => note.id === args.id);
-        }
-    },
-    Mutation: {
-        newNote: (parent, args) => {
-            let noteValue = {
-                id: String(notes.length + 1),
-                content: args.content,
-                author: 'Adam Scott'
-            };
-            notes.push(noteValue);
-            return noteValue;
+const getUser = token => {
+    if (token) {
+        try {
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            throw new Error('Session invalid');
         }
     }
 };
 
-const app = express();
-
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        const token = req.headers.authorization;
+        const user = getUser(token);
+        console.log(user);
+        return { models, user };
+    }
+});
 
 server.applyMiddleware({ app, path: '/api' });
 
-app.listen(port, () => console.log(`GraphQL Server runing at http://localhost:${port}${server.graphqlPath}!`));
+app.listen({ port }, () => console.log(`GraphQL Server runing at http://localhost:${port}${server.graphqlPath}!`));
